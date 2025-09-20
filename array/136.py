d47b620bd32bd62842d6dce6d729048052ac3ef8
Rishabh07-cell <rajsinghsingh8485@gmail.com>
2025-09-20 14:57:03 +0530

    Update 136.py

--- array/136.py
+++ array/136.py
@@ -1,9 +1,5 @@
 class Solution:
     def singleNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
         dic={}
         for num in nums:
             if not num in dic: dic[num]=1
