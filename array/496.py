580e61329d0bf94907d21edffb7a9f43850002b5
Rishabh07-cell <rajsinghsingh8485@gmail.com>
2025-09-21 16:05:18 +0530

    Create 496.py

---
+++ array/496.py
@@ -0,0 +1,8 @@
+class Solution:
+    def nextGreaterElement(self, nums1, nums2):
+        out=list()
+        for num in nums1: 
+            out.append(-1)
+            for j in range(nums2.index(num)+1,len(nums2)):
+                if nums2[j]>num: out[-1]=nums2[j]; break
+        return out
