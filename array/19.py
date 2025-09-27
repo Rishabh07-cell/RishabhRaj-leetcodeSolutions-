7b0466bb199a87d7e47e9055e00f4065e6aeebb1
Rishabh07-cell <rajsinghsingh8485@gmail.com>
2025-09-27 14:54:06 +0530

    Update and rename 19.py to temp

--- array/19.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def removeNthFromEnd(self, head, n):
-        dummy = ListNode(0)
-        dummy.next = head
-        arr = [dummy]
-        while head:
-            arr.append(head)
-            head = head.next
